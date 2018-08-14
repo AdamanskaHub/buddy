ee5e9f052dc4ffe5cf9f757db9a69fbd64aa836d
Emilie G <adamanska@gmail.com>
2018-08-14 17:53:07 +0200

    intro sentence ok, defining the others

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
 import React, { Component } from 'react';
 import './App.css';
 import {
-  MainBox, 
+  Main, Place, Chara, TextBox, Text,
 } from './components';
+import lady from './img/lady.png'
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.textChoice = this.textChoice.bind(this);
+    this.nextText = this.nextText.bind(this);
+
+    this.state = {
+      selected: "base",
+    };
+
+  }
+  componentWillMount() {
+    // Simulate API response
+    const speech = [ //smile
+      "Are you ready to give it your all?",
+      "Glad to see you're ready to work.",
+      "Less talk more action.",
+      "Now it's just about doing it.",
+    ]
+    this.setState({ speech })
+    const speech1 = [ 
+      "AAA",
+      "BBB",
+    ]
+    this.setState({ speech1 })
+    // Did you wake up early - moral / next
+    // Do some meditation - 
+    // Do plank
+    // Do some sport >> abs/stretching
+    // Green tea
+    // 
+  }
+
+  componentDidMount() {
+    this.textChoice("speech")
+  }
+
+  textChoice(choice) {
+    let random = this.state[choice][Math.floor(Math.random() * this.state[choice].length)];
+    this.setState({ selected: random })
+  }
+
+  nextText() {
+    let newText = this.state.speech+1;
+    this.setState({ selected: random })
+  }
+
+
+
   render() {
     return (
-      <MainBox>
-gzuhiu
-      </MainBox>
+      <Main>
+        <Place>
+          <Chara src={lady} />
+          {this.question ?
+            <div>
+              <Question>
+                <QuestionText>{this.state.questionA}</QuestionText>
+              </Question>
+              <Question>
+                <QuestionText>{this.state.questionB}</QuestionText>
+              </Question>
+            </div>
+            : null}
+          <TextBox onClick={this.nextText}>
+            <Text>{this.state.selected}</Text>
+          </TextBox>
+        </Place>
+      </Main>
     );
   }
 }
--- src/components.js
+++ src/components.js
@@ -1,7 +1,41 @@
 import styled, {keyframes} from 'styled-components';
 
-const MainBox = styled.h1`
+export const Main = styled.div`
+  min-height: 100vh;
   height: 100%;
   width: 100%;
   background-color: #f0f6ff;
+`;
+
+export const Place = styled.div`
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+  background-color: #f0f6ff;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Chara = styled.img`
+  align-self: center;
+  max-height: 70vh;
+`;
+
+export const TextBox = styled.div`
+  height: 20vh;
+  width: 80%;
+  background-color: #fff;
+  padding: 20px;
+  border: 2px pink solid;
+  margin-bottom: 5vh;
+`;
+
+export const Text = styled.p`
+  padding: 0;
+  margin: 0;
+  color: #333;
+  font-size: 1.3em;
+
 `;
