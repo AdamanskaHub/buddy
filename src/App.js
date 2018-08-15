fa441efbbae0f70f9f2fe3de5c17f091d165abf6
Emilie G <adamanska@gmail.com>
2018-08-15 17:24:08 +0200

    first interactions kinda working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {
-  Main, Place, Chara, TextBox, Text,
+  Main, Place, Top, Chara, TextBox, Text, QuestionContainer, Question, QuestionText,
 } from './components';
 import lady from './img/lady.png'
 
@@ -11,9 +11,19 @@ class App extends Component {
     super();
     this.textChoice = this.textChoice.bind(this);
     this.nextText = this.nextText.bind(this);
+    this.ask= this.ask.bind(this);
+    this.reply = this.reply.bind(this);
 
     this.state = {
       selected: "base",
+      question: false,
+      wakeUpBool: false,
+      speech: [],
+      wakeUp: [],
+      answerYes: [],
+      answerNo: [],
+      answerSoon: [],
+      intro: [],
     };
 
   }
@@ -26,21 +36,46 @@ class App extends Component {
       "Now it's just about doing it.",
     ]
     this.setState({ speech })
-    const speech1 = [ 
-      "AAA",
-      "BBB",
+    
+  }
+
+  componentDidMount() {
+    this.textChoice("speech")
+
+    // WAKE UP
+    const wakeUp = [
+      "When did you wake up?",
+      "You woke up early, didn't you?",
     ]
-    this.setState({ speech1 })
+    this.setState({ wakeUp })
+
+    const answerYes = [
+      "That's great. Now don't ruin it!",
+      "Seize the day!",
+    ]
+    this.setState({ answerYes })
+
+    const answerNo = [
+      "I didn't do shit",
+      "I behave like a larvae",
+    ]
+    this.setState({ answerNo })
+    
+    const answerSoon = [
+      "On my way",
+      "Doing it right now",
+    ]
+    this.setState({ answerSoon })
+    // MEDITATION
+
     // Did you wake up early - moral / next
     // Do some meditation - 
     // Do plank
     // Do some sport >> abs/stretching
     // Green tea
-    // 
-  }
-
-  componentDidMount() {
-    this.textChoice("speech")
+    // fruit
+    // apprendre
+    // lire
   }
 
   textChoice(choice) {
@@ -48,28 +83,80 @@ class App extends Component {
     this.setState({ selected: random })
   }
 
-  nextText() {
-    let newText = this.state.speech+1;
-    this.setState({ selected: random })
+  ask() {
+    this.setState({ question: true })
+    let randomYes = this.state.answerYes[Math.floor(Math.random() * this.state.answerYes.length)];
+    this.setState({ answerYes: randomYes })
+    let randomNo = this.state.answerYes[Math.floor(Math.random() * this.state.answerNo.length)];
+    this.setState({ answerNo: randomNo })
+    let randomSoon = this.state.answerSoon[Math.floor(Math.random() * this.state.answerSoon.length)];
+    this.setState({ answerSoon: randomSoon })
   }
 
+  nextText() {
+    console.log("next text")
+    if (!this.state.wakeUpBool) {
+      this.textChoice("wakeUp");
+      this.setState({ wakeUpBool: true })
+      console.log(this.state.wakeUpBool)
+    } if (this.state.wakeUpBool && !this.state.question) {
+      this.ask();
+    } else {
+      console.log("next text error")
+    }
+  }
+
+  reply(replyIs) {
+    if (replyIs = "Yes") {
+      console.log("nReplied Yes")
+    } 
+    if (replyIs = "No") {
+      console.log("nReplied No")
+    }
+    if (replyIs = "Soon") {
+      console.log("nReplied Soon")
+    } else {
+      console.log("nextText error")
+    }
+  }
+
+  // updateText(whereAt) {
+  //   let x = whereAt
+  //   switch (x) {
+  //     case "wakeUp":
+  //       this.textChoice("wakeUp")
+  //       break;
+      
+  //     default:
+  //       console.log("default switch");
+  //   }
+  // }
 
 
   render() {
     return (
       <Main>
         <Place>
-          <Chara src={lady} />
-          {this.question ?
-            <div>
-              <Question>
-                <QuestionText>{this.state.questionA}</QuestionText>
-              </Question>
-              <Question>
-                <QuestionText>{this.state.questionB}</QuestionText>
-              </Question>
-            </div>
-            : null}
+          <Top>
+            <Chara src={lady} visibility={this.state.question ? "0.5" : "1"} />
+            {this.state.question ?
+              <QuestionContainer>
+                <Question>
+                  <QuestionText 
+                  // onClick={this.reply("Yes")}
+                  >{this.state.answerYes}</QuestionText>
+                </Question>
+                <Question>
+                  <QuestionText //onClick={this.reply("No")}
+                  >{this.state.answerNo}</QuestionText>
+                </Question>
+                <Question>
+                  <QuestionText // onClick={this.reply("Soon")}
+                  >{this.state.answerSoon}</QuestionText>
+                </Question>
+              </QuestionContainer>
+              : null}
+          </Top>
           <TextBox onClick={this.nextText}>
             <Text>{this.state.selected}</Text>
           </TextBox>
--- src/components.js
+++ src/components.js
@@ -18,10 +18,56 @@ export const Place = styled.div`
   align-items: center;
 `;
 
+export const Top = styled.div`
+  height:79vh;
+  background-color: purple;
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
+
 export const Chara = styled.img`
   align-self: center;
-  max-height: 70vh;
+  max-height: 100%
+  opacity: ${ props => props.visibility}
 `;
+
+export const QuestionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  width: 100%;
+    height: 100%;
+    top: 0;
+`;
+
+export const Question = styled.div`
+  min-width: 25%;
+  background-color: #fff;
+  padding: 20px;
+  border: 2px pink solid;
+  margin: 10px 0;
+
+  &:hover {
+    background-color: pink; 
+    &>span {
+        color: #fff;
+    }
+  }
+
+`;
+
+export const QuestionText = styled.span`
+    padding: 0;
+    margin: 0;
+    color: #333;
+    font-size: 1.3em;
+`;
+
+
 
 export const TextBox = styled.div`
   height: 20vh;
@@ -29,7 +75,8 @@ export const TextBox = styled.div`
   background-color: #fff;
   padding: 20px;
   border: 2px pink solid;
-  margin-bottom: 5vh;
+  margin-bottom: 10px;
+  box-sizing: border-box;
 `;
 
 export const Text = styled.p`
